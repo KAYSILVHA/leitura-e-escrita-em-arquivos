0de99ff041688451bd8950818de1fc9dacf8bcdd
KAYSILVHA <kaysilvha@gmail.com>
2023-09-20 21:58:45 -0300

    trabalhando com promessas

--- scripts.js
+++ scripts.js
@@ -1,22 +1,20 @@
-const fs = require('fs')
+const fsPromises = require('fs/promises')
 
-//leitura de arquiuvos sincrono
-// console.log('antes de ler');
-
-// const a = fs.readFileSync('a.txt').toString();
-// console.log(a)
-
-// console.log('depois de ler')
-
-//leitura de arquiuvos assincrono
 console.log('antes de ler');
 
-fs.readFile('a.txt', (erro, data)=>{
-  if(erro){
-    console.log(erro);
-  }else{
-    console.log(data.toString());
-  }
-})
+// const a = fsPromises.readFile('a.txt');
+// a.then(data=>{
+//   console.log(data.toString())
+// });
+
+// a.catch(erro=>{
+//   console.log(erro)
+// })
+
+(async function(){
+  const a = await fsPromises.readFile('a.txt');
+
+  console.log(a.toString());
+})();
 
 console.log('depois de ler')
